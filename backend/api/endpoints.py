6c5053c988e9f1c7a0cb140a3f3aed25f077acd6
Erico Labare <erico.labare@outlook.com>
2025-09-02 16:56:32 +0200

    chore: instruction in doc + misc clean some folders + comment endpoint and details respond of backend( with exemple in docs)

--- backend/api/endpoints.py
+++ backend/api/endpoints.py
@@ -5,37 +5,129 @@ from parsing.pricer.poeninja_pricer import add_prices_to_json
 
 router = Router()
 
-@router.post("/decode", response=PobDecodeResponseAny)
+@router.post("/decode", response=PobDecodeResponseAny, tags=["Parser"])
 def decode_pob_endpoint(request, payload: PobDecodeRequest):
     """
-    Décode un PoB string envoyé dans le body JSON et retourne un JSON structuré.
+    Decode a PoB string sent in the JSON body and return a structured JSON.
+    General skeleton of the response ( a complete exemple is given in the folder "route_response_exemple", in the "parser_exemple.json" file):
+    {
+      "class": "", # The class of the character
+      "ascendClass": "", # The ascendancy class of the character
+      "items": [
+        {
+          "name": "", # Item name
+          "itemBase": "", # Type of base
+          "rarity": "UNIQUE | RARE | MAGIC | NORMAL", # Rarity of the item
+          "properties": [ # There is a lot of different properties, this is just an example of the most common
+            {
+              "name": "Unique ID",
+              "values": "" # hash_or_id_string unique to each item
+            },
+            {
+              "name": "Item Level",
+              "values": "" # Level of the item
+            },
+            {
+              "name": "LevelReq",
+              "values": "" # Level requirement of the item
+            },
+            {
+              "name": "Implicits",
+              "values": "" # Number of implicits modifiers
+            },
+            {
+              "name": "Corrupted",
+              "values": "True | False" # Corrupted or not
+            }
+          ],
+          "implicitMods": [
+            "" # List of implicit modifiers
+          ],
+          "explicitMods": [
+            "" # Liste of explicit modifiers
+          ],
+          "type": "Weapon | Armor | Jewelry | Jewel | Flask", # Type of the item
+          "subType": "" # Subtype depending on type (e.g. Bow, Ring, Cluster Jewel)
+          "5/6Link" # Check if item is five or six linked
+        }
+      ],
+      "skills": [
+        {
+          "slot": "Body Armour | Gloves | Helmet | Weapon 1 | Weapon 2 | Jewelry | Jewel | Flask", # Slot where the gem is located
+          "gems": [
+            {
+              "count": "", # Number of gems in the slot (always 1 in this version)
+              "level": integer, # Level of the gem
+              "enabled": true, # Activated of not (mostly irrelevant)
+              "quality": integer, # Quality of the gem
+              "qualityId": "Default | Alternate", # Qualtity of the gem (not used as of modern PoE)
+              "skillId": "InternalGemID", # Internal ID of the gem
+              "variantId": "VariantName", # Variant of the gem
+              "enableGlobal1": true | false, # ? variable
+              "enableGlobal2": true | false, # ? variable, false for item only skills
+              "nameSpec": "Display Name of the Gem", # Display name of the gem
+              "gemId": "FullMetadataPath", # Full metadata path of the gem
+              "Corrupted": true | false # Infered data on, if  the gem is corrupted or not
+            }
+          ]
+        }
+      ],
+      "linksBySlot": {
+        "Slot" : "", # Links of a PoB slot on the form "O-O-O-O O-O" = 6 sockets, 1 groupe of 4 linked and 1 group of 2 linked
+      }
+    }
     """
     try:
-        #  Décoder le code PoB en XML
+        # Decode the PoB code into XML
         pob_xml = decode_pob_code(payload.pob_string)
 
-        # Convertir le XML en JSON structuré
+        # Convert the XML into structured JSON
         parsed_data = pob_xml_to_json(pob_xml)
 
         return {"data": parsed_data}
 
     except Exception as e:
-        # On renvoie un message d'erreur simple
+        # Return a simple error message
         return {"data": {"error": str(e)}}
 
-@router.post("/price", response=PobPriceResponseAny)
+
+@router.post("/price", response=PobPriceResponseAny, tags=["Pricer"])
 def price_pob_endpoint(request, payload: PobDecodeRequest):
     """
-    Décode un PoB string dans le body JSON et retourne un JSON structuré et ajoute les prix via add_prices_to_json().
+    Decode a PoB string from the JSON body, convert it into structured JSON,
+    and enrich it with prices using add_prices_to_json().
+    General skeleton of the response is the same as the one from /decode route, but with some things added(a complete exemple is given in the folder "route_response_exemple", in the "pricer_exemple.json" file):
+        {
+      "items": [
+        {
+          "priceChaos": float, # Price of the item in chaos
+          "priceDivine": float, # Price of the item in divine
+        }
+      ],
+      "skills": [
+        {
+          "gems": [
+            {
+              "priceChaos": float, # Price of the gem in chaos
+              "priceDivine": float, # Price of the gem in divine
+              "matchedLevel": integer, # Level of gem if there a fallback
+              "matchedQuality": integer, # Quality of gem if there a fallback
+              "fallbackUsed": true | false # True if a fallback was used in case of original item not found
+            }
+          ]
+        }
+      ],
+    }
     """
     try:
-        # Décoder le PoB en JSON
+        # Decode the PoB into JSON
         pob_xml = decode_pob_code(payload.pob_string)
         parsed_data = pob_xml_to_json(pob_xml)
 
-        # Ajouter les prix
+        # Add pricing data
         priced_data = add_prices_to_json(parsed_data)
 
         return {"data": priced_data}
+
     except Exception as e:
         return {"data": {"error": str(e)}}
---
+++ docs/backend/Instructions and help.md
@@ -0,0 +1,44 @@
+### 1. Doc API
+
+
+### 2. Basic instructions on launching BACKEND server locally :
+
+Launch those commands :
+- ````cd backend````: Go to backend folder
+- ````python -m venv venv````: Create a virtual environment
+- ````source venv/bin/activate````: Activate the virtual environment
+- ou ````.\venv\Scripts\activate```` on Windows
+- ````pip install -r requirements.txt````: Install dependencies
+- ````python manage.py migrate````: Migrate the database from "migrations" files
+- ````python manage.py runserver````: Launch the server
+
+
+### 3. Temporary setting for .env to launch PostgreSQL Serveur :
+- 
+- Always have PostgreSQL Launched
+- Create a PostgreSQL server locally
+- Add the following line in a file named ".env" in "backend/" :
+    ````
+    # Database configuration with PostgreSQL
+    POSTGRES_HOST=localhost
+    POSTGRES_DB=your_db
+    POSTGRES_USER=your_user
+    POSTGRES_PASSWORD=your_password
+    POSTGRES_PORT=5432
+    ````
+Configure your .env as you need, in particular :
+- "POSTGRES_DB": the name of your database created in Postgre
+- "POSTGRES_USER": the name of the user used to connect to your database
+- "POSTGRES_PASSWORD": the password of the user used to connect to your database
+
+### 4. Test
+
+````python -m pytest -v````: run test from backend
+
+### 5. Useful commands :
+
+- ````python manage.py makemigrations````: create migration files for your models to apply to database
+- ````python manage.py createsuperuser````: create a superuser to access to admin page
+
+### 6. Example of response from routes :
+Two examples of response from routes are given in the folder "route_response_exemple". They correspond to the response of the routes "price_pob" and "decode_pob", name in the endpoint "Pricer" and "Parser".
